97b13b914fedb1a24d023bbe29f1ae6087a316a0
gabivieiraa <159184847+gabivieiraa@users.noreply.github.com>
2024-10-01 16:40:01 -0300

    01-10

---
+++ assets/pergunta.js
@@ -0,0 +1,18 @@
+criaCartao(
+    "programacao",
+    "O que é Python",
+    "O Python é uma linguagem de programação",
+
+    )
+    criaCartao(
+    "geografia",
+    "Qual é a capital da França",
+    "A capital da França é París",
+
+    )
+    criaCartao(
+        "programação",
+        "O que é uma função",
+        "Uma função é um bloco de código que executa alguma tarefa",
+
+    )
